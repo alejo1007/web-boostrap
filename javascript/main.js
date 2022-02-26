5906d44921c000751069aebdfb926d90bccc009a
alejo1007 <68398182+alejo1007@users.noreply.github.com>
2022-02-26 14:21:53 -0300

    Primer repositorio boostrap paginas muestra

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+var myCarousel = document.querySelector("#myCarousel");
+var carousel = new bootstrap.Carousel(myCarousel);
+var myCarousel = document.querySelector("#myCarousel");
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false,
+});
+import "bootstrap/dist/css/bootstrap.min.css";
